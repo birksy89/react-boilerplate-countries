0dc1c6833514d46e8f56a4beb53a2f441b8d238e
Andrew <andrew@purplecs.com>
2016-09-05 16:01:30 +0100

    Adding flags via NPM

--- app/components/country.jsx
+++ app/components/country.jsx
@@ -1,13 +1,16 @@
 var React = require('react');
-
+var numeral = require('numeral');
 module.exports = React.createClass({
 
     render: function() {
         var activeCountry = this.props.countryData;
+
+        var popString = numeral(activeCountry.population).format('0,0');
         return (
             <div className="card card-block">
                 <h1>{activeCountry.name}</h1>
-                <p>Population: {activeCountry.population}</p>
+                <p>Population: {popString}</p>
+                <span className="flag-icon flag-icon-gr"></span>
             </div>
 
         )
